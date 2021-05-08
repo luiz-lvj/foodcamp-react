f10c678916e4c4169a3b5654cd35e78c62906ef5
Luiz <lvj253@gmail.com>
2021-05-08 19:02:59 -0300

    layout card de produtos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom';
 import TopPage from './components/TopPage';
 import BottomButton from './components/BottomButton';
+import Products from './components/Products';
+import './reset.css';
+import './styles.css';
 
 export default function App(){
     return(
         <>
         <TopPage/>
+        <Products/>
         <BottomButton/>
         </>
     );
