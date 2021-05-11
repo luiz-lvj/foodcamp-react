6c52aaa292f8c5b38d5639e9736674a86145b6a3
ronald <ronaldjunior2@hotmail.com>
2021-05-11 15:54:58 -0300

    preparando review

--- src/components/TopPage.js
+++
@@ -1,8 +0,0 @@
-export default function TopPage(){
-    return(
-        <div className="top">
-            <h1>FoodCamp</h1>
-            <h2>Sua comida em 6 minutos</h2>
-        </div>
-    );
-}
